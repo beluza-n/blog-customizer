0e7f31fb12d7d6b96908a72a41f86fc2a41b82ee
beluza <beluza@mail.ru>
2026-09-20 00:06:27 +0300

    закритие окна при щелчке снаружи формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,21 +1,29 @@
 import { clsx } from 'clsx';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = (): React.JSX.Element => {
   const [isOpen, setIsOpen] = useState(false)
+  const rootRef = useRef<HTMLDivElement>(null)
 
   const handleArrowClick = (): void => {
     setIsOpen(!isOpen);
   }
 
+  useOutsideClickClose({
+    isOpen,
+    rootRef,
+    onChange: setIsOpen,
+  })
+
   return (
     <>
       <ArrowButton isOpen={isOpen} onClick={handleArrowClick} />
-      <aside className={clsx(styles.container, isOpen && styles.container_open)}>
+      <aside ref={rootRef} className={clsx(styles.container, isOpen && styles.container_open)}>
         <form className={styles.form}>
           <div className={styles.bottomContainer}>
             <Button title="Сбросить" htmlType="reset" type="clear" />
